0355e17a3b4e27cdd19ec4a5fcaf34f7b906d8a4
Angelo Totanes <hazukashi_gariyasan79@yahoo.com>
2020-02-27 16:16:12 -0800

    Added save to jpeg feature

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// import SignaturePad from 'signature_pad/dist/signature_pad';
+
 var canvas = document.getElementById('signature-pad');
 
 // Adjust canvas coordinate space taking into account pixel ratio,
@@ -16,6 +18,42 @@ function resizeCanvas() {
 window.onresize = resizeCanvas;
 resizeCanvas();
 
+function download(dataURL, filename) {
+  if (navigator.userAgent.indexOf("Safari") > -1 && navigator.userAgent.indexOf("Chrome") === -1) {
+    window.open(dataURL);
+  } else {
+    var blob = dataURLToBlob(dataURL);
+    var url = window.URL.createObjectURL(blob);
+
+    var a = document.createElement("a");
+    a.style = "display: none";
+    a.href = url;
+    a.download = filename;
+
+    document.body.appendChild(a);
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+  }
+}
+
+// One could simply use Canvas#toBlob method instead, but it's just to show
+// that it can be done using result of SignaturePad#toDataURL.
+function dataURLToBlob(dataURL) {
+  // Code taken from https://github.com/ebidel/filer.js
+  var parts = dataURL.split(';base64,');
+  var contentType = parts[0].split(":")[1];
+  var raw = window.atob(parts[1]);
+  var rawLength = raw.length;
+  var uInt8Array = new Uint8Array(rawLength);
+
+  for (var i = 0; i < rawLength; ++i) {
+    uInt8Array[i] = raw.charCodeAt(i);
+  }
+
+  return new Blob([uInt8Array], { type: contentType });
+}
+
 var signaturePad = new SignaturePad(canvas, {
   backgroundColor: 'rgb(255, 255, 255)' // necessary for saving image as JPEG; can be removed is only saving as PNG or SVG
 });
@@ -30,14 +68,15 @@ document.getElementById('save-png').addEventListener('click', function () {
   window.open(data);
 });
 
-document.getElementById('save-jpeg').addEventListener('click', function () {
+document.getElementById('save-jpeg').addEventListener('click', function (e) {
+  e.preventDefault();
   if (signaturePad.isEmpty()) {
     return alert("Please provide a signature first.");
   }
 
   var data = signaturePad.toDataURL('image/jpeg');
   console.log(data);
-  window.open(data);
+  download(data, "signature.jpg");
 });
 
 document.getElementById('save-svg').addEventListener('click', function () {
